df791960ffc1437da5471578b202010cca627809
guruuu121-creator <guruuu121@gmail.com>
2026-06-21 18:01:44 +0530

    [C2G] Submit: Coding Assignment - Question 3

---
+++ assignments/Coding Assignment/Question 3/question.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Write your solution here
+        int t = sc.nextInt();
+        if(t>=30){
+            System.out.print("Fan ON");
+        }else{
+            System.out.print("Fan OFF");
+        }
+    }
+}
