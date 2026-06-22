0271353842ffe3339f539facc3ca6a0153dfad7a
guruuu121-creator <guruuu121@gmail.com>
2026-06-22 08:23:59 +0530

    [C2G Practice] Solved: Sort Colors

---
+++ practice/medium/sort-colors/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Write your solution here
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
