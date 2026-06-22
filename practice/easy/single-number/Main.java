51b7dd8133925091058eac956c49d9ca2ea277d4
guruuu121-creator <guruuu121@gmail.com>
2026-06-22 09:42:22 +0530

    [C2G Practice] Solved: Single Number

---
+++ practice/easy/single-number/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Write your solution here
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans = ans ^ a[i];
+        }
+        System.out.print(ans);
+    }
+}
