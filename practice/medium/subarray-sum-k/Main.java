c8b7518cbb0497a3c32ead88763f4ca5097b4471
guruuu121-creator <guruuu121@gmail.com>
2026-06-25 10:46:32 +0530

    [C2G Practice] Solved: Subarray Sum Equals K

---
+++ practice/medium/subarray-sum-k/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Write your solution here
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int count = 0;
+        for(int i=0;i<n;i++){
+            int sum= 0;
+            for(int j=i;j<n;j++){
+                sum += arr[j];
+                if(sum==k){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
