9740dff8c9664af0107fae8732eab6557fe30aa9
guruuu121-creator <guruuu121@gmail.com>
2026-07-02 08:20:48 +0530

    [C2G Practice] Solved: Reverse String

--- practice/easy/reverse-string/Main.java
+++ practice/easy/reverse-string/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // Write your solution here
-        String s = sc.nextLine();
+        String s =sc.next();
         String rev = "";
         for(int i=s.length()-1;i>=0;i--){
             rev = rev + s.charAt(i);
