8668d6835538600ae7a1f5322f17e3c15ac3cedc
guruuu121-creator <guruuu121@gmail.com>
2026-07-02 08:47:48 +0530

    [C2G Practice] Solved: Fibonacci Number

--- practice/easy/fibonacci-number/Main.java
+++ practice/easy/fibonacci-number/Main.java
@@ -4,22 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // Write your solution here
-        int n = sc.nextInt();
-        if(n==0){
-            System.out.println(0);
-            return;
-        }
-        if(n==1){
-            System.out.println(1);
-            return;
-        }
-        int a=0;
-        int b=1;
-        for(int i=2;i<=n;i++){
-            int c = a+b;
-            a = b;
-            b = c;
-        }
-        System.out.println(b);
+       int n = sc.nextInt();
+       if(n==0){
+        System.out.println(0);
+        return;
+       } 
+       if(n==1){
+        System.out.println(1);
+        return;
+       }
+       int a=0;
+       int b=1;
+       for(int i=2;i<n;i++){
+        int c = a+b;
+        a=b;
+        b=c;
+       }
+       System.out.println(b);
     }
 }
