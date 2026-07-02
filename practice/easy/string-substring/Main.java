298450658e43969088ec14fa73a09ab5cb5eb8a3
guruuu121-creator <guruuu121@gmail.com>
2026-07-02 08:10:49 +0530

    [C2G Practice] Solved: String Substring

---
+++ practice/easy/string-substring/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Write your solution here
+        String s = sc.next();
+        String sub = s.substring(1,3);
+        System.out.println(sub);
+    }
+}
